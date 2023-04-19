39753d312e5fcf2d33fe2ad7c469b6009cd16b96
1bless <dri6lees@gmail.com>
2023-04-19 23:26:47 +0200

    updated

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import os
+import json
+import openai
+import speech_recognition as sr
+from gtts import gTTS
+from io import BytesIO
+from pydub import AudioSegment
+from pydub.playback import play
+
+import os
 import openai
 import speech_recognition as sr
 from gtts import gTTS
@@ -30,10 +39,6 @@ class SpeechRecognizer:
         except sr.RequestError as e:
             print(f"Could not request results; {e}")
             return None
-
-    def save_to_file(self, text, file_path="output.txt"):
-        with open(file_path, "a") as f:
-            f.write(text + "\n")
 
 class TextToSpeech:
     def __init__(self, language="en", slow=False):
@@ -74,16 +79,27 @@ class ChatBot:
 
 
 if __name__ == "__main__":
-    output_file = "output.txt"
-    if os.path.exists(output_file):
-        os.remove(output_file)
-
     speech_recognizer = SpeechRecognizer()
     tts = TextToSpeech()
 
-    role = "You are addicted to crack-cocaine. You act, respond and behave like it from now on. Every input you get, you treat as if the user is a random person approaching you, the crackhead, in public where you are currently begging for money to buy some more crack, which you dont directly admit, but after a short conversation you do. You are also mentally affected by the years of drug use, so you behave a bit crazy. Just like Tyrone Biggums from the Chappelles Show. You have the ongoing charisma and jokes which dave chappelles character tyrone biggums has. Also, you are very unlikely to be convinced of stopping crack. Actually, you sometimes act as if you agree, feel bad and want to stop - but if you do, its just another backstab move you do to get some money for crack by lying."
-    
-    bot = ChatBot(role)
+    script_path = os.path.dirname(os.path.realpath(__file__)) # path of this script
+    roles_file  = os.path.join(script_path, 'roles.json') # path of the json file, containing the roles
+    with open(roles_file, 'r') as file:
+        roles_data = json.load(file) # roles_data is now a dictionary
+
+    print("Available roles:")
+    for role in roles_data:
+        print(role)
+
+    while True:
+        bot_name = input("Enter role for bot: ")
+        if bot_name in roles_data:
+            role = roles_data[bot_name]
+            bot = ChatBot(role)
+            break
+        else:
+            print("Invalid role. Please try again.")
+
 
     while True:
         recognized_text = speech_recognizer.recognize_speech()
@@ -91,10 +107,9 @@ if __name__ == "__main__":
             if "terminate" in recognized_text.lower():
                 print("Terminating program...")
                 break
-            speech_recognizer.save_to_file(recognized_text, output_file)
             response = bot.get_response(recognized_text)
             if response:
-                print(f"Assistant: {response}")
+                print(f"{bot_name}: {response}")
                 tts.speak(response)
             else:
-                print("Assistant: No response")
+                print(f"{bot_name}: No response")
